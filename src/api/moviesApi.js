2f7881db4bd9d8006906e0880236ea87c3fe882b
Alona-Suprun <alonasuprun.l@gmail.com>
2021-08-24 16:46:14 +0300

    add api

---
+++ src/api/moviesApi.js
@@ -0,0 +1,13 @@
+const URL = "https://api.themoviedb.org/3";
+const API_KEY = "a5c011004da0764d53894c4d728d240c";
+
+async function fetchMovies(url = "", config = {}) {
+  const response = await fetch(url, config);
+  return response.ok
+    ? await response.json()
+    : Promise.reject(new Error("Not found"));
+}
+
+export function fetchTrendingMovies() {
+  return fetchMovies(`${URL}/trending/movie/day?api_key=${API_KEY}`);
+}
