442d880d01e5456cd5b7f37ccb713a02cfa3627c
Alona-Suprun <alonasuprun.l@gmail.com>
2021-08-26 15:43:02 +0300

    add Cast component

---
+++ src/components/Cast/Cast.js
@@ -0,0 +1,40 @@
+import { useState, useEffect } from "react";
+
+import * as moviesApi from "../../api/moviesApi";
+import img from "../../images/camera.svg";
+import s from "../Cast/Cast.module.css";
+
+const Cast = ({ movieId }) => {
+  const [movieCast, setMovieCast] = useState(null);
+
+  useEffect(() => {
+    moviesApi.fetchMovieCast(movieId).then((movie) => setMovieCast(movie.cast));
+  }, [movieId]);
+
+  return (
+    <>
+      {movieCast && (
+        <ul className={s.actors}>
+          {movieCast.map((cast) => (
+            <li key={cast.id} className={s.actor}>
+              <img
+                className={s.actorImage}
+                src={
+                  cast.profile_path === null
+                    ? img
+                    : `https://image.tmdb.org/t/p/original${cast.profile_path}`
+                }
+                alt={cast.name}
+              />
+              <span>{cast.name}</span>
+              <br />
+              Character: {cast.character}
+            </li>
+          ))}
+        </ul>
+      )}
+    </>
+  );
+};
+
+export default Cast;
